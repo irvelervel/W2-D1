4715618e4c03c20603e6df128072f3c28974bfe6
Stefano Casasola <ste.casasola@gmail.com>
2025-02-10 09:52:34 +0100

    Creates a JS file

---
+++ script.js
@@ -0,0 +1,11 @@
+// QUI DENTRO SIAMO IN UN FILE SEPARATO!
+// OTTIMO PER SCRIVERE DEL JS IN MODO ORDINATO, SENZA
+// APPESANTIRE LA PAGINA HTML
+// INOLTRE SARÀ POSSIBILE APPLICARE QUESTO FOGLIO JS A PIÙ PAGINE HTML
+
+// questo foglio JS è collegato ad index.html grazie ad un tag <script>
+// questo significa che all'avvio della pagina index.html verrà eseguito anche
+// questo documento JS
+
+// proviamo a scrivere un messaggio in console!
+console.log('Hello World!');
